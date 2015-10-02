c045cbaa81461f94d8bae50a2d1a65bd716d619f
Jonathan Ward <jonathan.stuart.ward@gmail.com>
2015-10-02 15:45:56 +0100

    Fixed redis URL

--- output/outputRedis.go
+++ output/outputRedis.go
@@ -9,7 +9,7 @@ import "github.com/mediocregopher/radix.v2/redis"
 //WriteRedis writes buckets to a Redis list. Buckets are encoded
 //as JSON. Returns err if a write fails.
 func WriteRedis(buckets []Bucket, redisURL url.URL) error {
-	redisClient, redisErr := redis.Dial("tcp", "localhost:6379")
+	redisClient, redisErr := redis.Dial("tcp", redisURL.String())
 	if redisErr != nil {
 		return redisErr
 	}
@@ -20,7 +20,6 @@ func WriteRedis(buckets []Bucket, redisURL url.URL) error {
 		if jsonErr == nil {
 			redisClient.PipeAppend("APPEND", "aggregated-failed", jsonBucket)
 		}
-
 	}
 
 	response := redisClient.ReadResp()
