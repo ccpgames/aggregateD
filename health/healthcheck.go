84bf5fc8d93e11e2f448e9ecccbcbc1d18ba1966
Jonathan Ward <jonathan.stuart.ward@gmail.com>
2016-04-27 11:58:15 -0700

    Fixed multiple header issue

--- health/healthcheck.go
+++ health/healthcheck.go
@@ -13,11 +13,12 @@ type healthHTTPHandler struct {
 
 func (handler *healthHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err := http.Get(handler.influxdbConfig.InfluxURL + "/ping")
+	log.Println(handler.influxdbConfig.InfluxURL)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Unable to write to InfluxDB"))
+		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("aggregateD is healthy"))
 }
@@ -26,6 +27,7 @@ func (handler *healthHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 func Serve(influxdbConfig output.InfluxDBConfig) {
 	server := http.NewServeMux()
 	handler := new(healthHTTPHandler)
+	handler.influxdbConfig = influxdbConfig
 	server.Handle("/health", handler)
 	log.Printf("Serving Healthcheck on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", server))
