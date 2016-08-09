68c5ef5cb73bd93f0601503af11bf9012597210b
Jonathan Ward <jonathan.stuart.ward@gmail.com>
2016-08-09 15:18:28 -0700

    Added batched metrics endpoint

--- input/inputJSON.go
+++ input/inputJSON.go
@@ -29,6 +29,11 @@ type (
 		Tags          map[string]string
 	}
 
+	//MetricBatch represent a batch of individual metrics that have been sent together
+	MetricBatch struct {
+		batch []Metric
+	}
+
 	//Event represents a single event instance
 	Event struct {
 		Name           string
@@ -49,6 +54,10 @@ type (
 	eventsHTTPHandler struct {
 		eventsIn chan Event
 	}
+
+	metricsBatchHTTPHandler struct {
+		metricsIn chan Metric
+	}
 )
 
 //http handler function, unmarshalls json encoded metric into metric struct
@@ -61,23 +70,7 @@ func (handler *metricsHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 	log.Printf("Received metric from %s\n", sourceIP)
 
 	if err == nil {
-		//add an aditional field specifing the host which forwarded aggregateD the metric
-		//this might often be the same as the client specified host field but in situations
-		//where the client is behind NAT, i.e many EVE clients this information is useful.
-		if receivedMetric.SecondaryData == nil {
-			receivedMetric.SecondaryData = make(map[string]interface{})
-		}
-		receivedMetric.SecondaryData["source"] = sourceIP
-
-		//ensure that no secondary values are nil, clients should not
-		//submit nil values but if they do they should not be sent to Influx
-		for k := range receivedMetric.SecondaryData {
-			if receivedMetric.SecondaryData[k] == nil {
-				receivedMetric.SecondaryData[k] = 0.0
-			}
-		}
-
-		handler.metricsIn <- receivedMetric
+		parseMetric(receivedMetric, sourceIP, handler.metricsIn)
 	} else {
 		log.Println(err)
 		log.Printf("Unable to decode metric from, %s", sourceAddress)
@@ -86,8 +79,8 @@ func (handler *metricsHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 	r.Body.Close()
 }
 
-//unmarshall json encoded events into event struct
 func (handler *eventsHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//unmarshall json encoded events into event struct
 	decoder := json.NewDecoder(r.Body)
 	var receivedEvent Event
 	err := decoder.Decode(&receivedEvent)
@@ -99,14 +92,56 @@ func (handler *eventsHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 			receivedEvent.Tags = make(map[string]string)
 		}
 
+		//append source address to metric
 		receivedEvent.Tags["source"] = sourceIP
 		handler.eventsIn <- receivedEvent
 	} else {
+		//if unable to parse the metric, drop it. This could be a problem for out of date clients.
 		log.Println(err)
 		log.Printf("Unable to decode event from, %s", sourceAddress)
 	}
 
 	r.Body.Close()
+}
+
+func (handler *metricsBatchHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var receivedMetricBatch MetricBatch
+	err := decoder.Decode(&receivedMetricBatch)
+	sourceAddress := r.RemoteAddr
+	sourceIP, _, _ := net.SplitHostPort(r.RemoteAddr)
+	log.Printf("Received metric from %s\n", sourceIP)
+
+	if err == nil {
+		for i := range receivedMetricBatch.batch {
+			parseMetric(receivedMetricBatch.batch[i], sourceIP, handler.metricsIn)
+		}
+
+	} else {
+		log.Println(err)
+		log.Printf("Unable to decode metric batch from, %s", sourceAddress)
+	}
+
+}
+
+func parseMetric(receivedMetric Metric, sourceIP string, metricsIn chan Metric) {
+	//add an aditional field specifing the host which forwarded aggregateD the metric
+	//this might often be the same as the client specified host field but in situations
+	//where the client is behind NAT, i.e many EVE clients this information is useful.
+	if receivedMetric.SecondaryData == nil {
+		receivedMetric.SecondaryData = make(map[string]interface{})
+	}
+	receivedMetric.SecondaryData["source"] = sourceIP
+
+	//ensure that no secondary values are nil, clients should not
+	//submit nil values but if they do they should not be sent to Influx
+	for k := range receivedMetric.SecondaryData {
+		if receivedMetric.SecondaryData[k] == nil {
+			receivedMetric.SecondaryData[k] = 0.0
+		}
+	}
+
+	metricsIn <- receivedMetric
 }
 
 //ServeHTTP exposes /events and /metrics and proceses JSON encoded events
@@ -119,8 +154,12 @@ func ServeHTTP(port string, metricsIn chan Metric, eventsIn chan Event) {
 	eventsHandler := new(eventsHTTPHandler)
 	eventsHandler.eventsIn = eventsIn
 
+	metricsBatchHandler := new(metricsBatchHTTPHandler)
+	metricsBatchHandler.metricsIn = metricsIn
+
 	server.Handle("/metrics", metricsHandler)
 	server.Handle("/events", eventsHandler)
+	server.Handle("/metrics_batch", metricsBatchHandler)
 
 	log.Printf("Accepting json metrics on port %s", port)
 
